e18149b90ae840255646c3eaa1150fab75e2b449
Philippe Meyer <70378769+philippemeyer5@users.noreply.github.com>
2021-07-27 17:51:51 +0200

    R file for the analysis

---
+++ olympic winners by age and sport.R
@@ -0,0 +1,30 @@
+#Olympic gold medal winners by age and sport
+#packages
+
+library(tidyverse)
+library(Rmisc)
+
+data <- read.csv("C:/Users/Iudex/Desktop/r experiments/Olympic sports by age/athlete_events.csv", header = TRUE)
+summary(data)
+
+#filter for gold medals and summer games
+datafiltered <- data %>% filter(Season == "Summer", Medal == "Gold", Age > 1)
+summary(datafiltered)
+
+#aggregate on sport level
+aggregate(datafiltered[,4], list(datafiltered$Sport), mean)
+#quite the difference between sports. How to plot it?
+
+#aggregate on year level
+aggregate(datafiltered[,4], list(datafiltered$Year), mean)
+#Not too much of a development here
+
+x <- summarySE(datafiltered, measurevar="Age", groupvars=c("Sex","Sport" ))
+x[is.na(x)] <- 0
+
+a <- ggplot(x,aes(x=Sport, y=Age, color=Sex)) + 
+      geom_errorbar(aes(ymin=Age-sd, ymax=Age+sd), width=.1) +
+      geom_line() +
+      geom_point()
+a + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
