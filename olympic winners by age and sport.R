fbef63d7980fad619fa1bd7d127505f0c32efa74
Philippe Meyer <70378769+philippemeyer5@users.noreply.github.com>
2021-07-27 18:16:48 +0200

    Added path for graph, better graph

--- olympic winners by age and sport.R
+++ olympic winners by age and sport.R
@@ -3,6 +3,8 @@
 
 library(tidyverse)
 library(Rmisc)
+install.packages('ggthemes', dependencies = TRUE)
+library(ggthemes)
 
 data <- read.csv("C:/Users/Iudex/Desktop/r experiments/Olympic sports by age/athlete_events.csv", header = TRUE)
 summary(data)
@@ -22,9 +24,15 @@ aggregate(datafiltered[,4], list(datafiltered$Year), mean)
 x <- summarySE(datafiltered, measurevar="Age", groupvars=c("Sex","Sport" ))
 x[is.na(x)] <- 0
 
-a <- ggplot(x,aes(x=Sport, y=Age, color=Sex)) + 
+png(file="C:/Users/Iudex/Desktop/r experiments/Olympic sports by age/age_sport.png",width=1280, height=720)
+ggplot(x,aes(x=Sport, y=Age, color=Sex)) + 
       geom_errorbar(aes(ymin=Age-sd, ymax=Age+sd), width=.1) +
       geom_line() +
-      geom_point()
-a + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      geom_point() +
+      theme_economist() +
+      scale_colour_economist() +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
+dev.off()
+
+#a + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
